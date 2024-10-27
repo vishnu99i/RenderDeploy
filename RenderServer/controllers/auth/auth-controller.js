4bb33a9b8dd42f8031317b3c8c98a96dafb7e84f
Vishnu P <vinventor1999@gmail.com>
2024-10-27 11:46:22 +0530

    Redirecting on refresh issue solving

--- RenderServer/controllers/auth/auth-controller.js
+++ RenderServer/controllers/auth/auth-controller.js
@@ -69,16 +69,30 @@ const loginUser = async (req, res) => {
     );
 
     //secure: false
-    res.cookie("token", token, { httpOnly: true, secure: true }).json({
-      success: true,
+    // res.cookie("token", token, { httpOnly: true, secure: true }).json({
+    //   success: true,
+    //   message: "Logged in successfully",
+    //   user: {
+    //     email: checkUser.email,
+    //     role: checkUser.role,
+    //     id: checkUser._id,
+    //     userName: checkUser.userName,
+    //   },
+    // });
+
+    res.status(200).json({
+      success: false,
       message: "Logged in successfully",
+      token,
       user: {
-        email: checkUser.email,
-        role: checkUser.role,
-        id: checkUser._id,
-        userName: checkUser.userName,
-      },
-    });
+            email: checkUser.email,
+            role: checkUser.role,
+            id: checkUser._id,
+            userName: checkUser.userName,
+          },
+    })
+
+
   } catch (e) {
     console.log(e);
     res.status(500).json({
@@ -98,8 +112,29 @@ const logoutUser = (req, res) => {
 };
 
 //auth middleware
+// const authMiddleware = async (req, res, next) => {
+//   const token = req.cookies.token;
+//   if (!token)
+//     return res.status(401).json({
+//       success: false,
+//       message: "Unauthorised user!",
+//     });
+
+//   try {
+//     const decoded = jwt.verify(token, "CLIENT_SECRET_KEY");
+//     req.user = decoded;
+//     next();
+//   } catch (error) {
+//     res.status(401).json({
+//       success: false,
+//       message: "Unauthorised user!",
+//     });
+//   }
+// };
+
 const authMiddleware = async (req, res, next) => {
-  const token = req.cookies.token;
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]
   if (!token)
     return res.status(401).json({
       success: false,
